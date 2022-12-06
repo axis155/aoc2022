d5a112b4646f80cf7b53551da9da9b79364539ce
Andrew Mowle <ajmowle@gmail.com>
2022-12-06 13:24:14 +0000

    finished day 6

---
+++ Day 6/day6.py
@@ -0,0 +1,28 @@
+#Part 1
+with open('data.txt') as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+
+packets = [lines[0][i] for i in range(len(lines[0]))]
+
+for i in range(len(packets)-3):
+    tempList = [packets[i + j] for j in range(4)]
+    if not set([x for x in tempList if tempList.count(x) > 1]):
+        print("Answer 1")
+        print(i + 4)
+        break
+
+#Part 2
+with open('data.txt') as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+
+packets = [lines[0][i] for i in range(len(lines[0]))]
+
+for i in range(len(packets)-13):
+    tempList = [packets[i + j] for j in range(14)]
+    #print(tempList)
+    if not set([x for x in tempList if tempList.count(x) > 1]):
+        print("Answer 2")
+        print(i + 14)
+        break
