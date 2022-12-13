5d410af41877c18070e6c20ef2e54e39bf97741a
Andrew Mowle <ajmowle@gmail.com>
2022-12-13 11:49:02 +0000

    set up day 11

---
+++ Day 11/day11.py
@@ -0,0 +1,8 @@
+#Part 1
+lines = [line.rstrip() for line in open("testdata.txt")]
+
+#set up monkey arrays with current items
+#loop through 20 times for each monkey
+#count number of movements for each monkey
+#show the monkeys with top 2 movements
+#multiple these two together for monkey business score
